8d64e620bcb773ea7478939e17f180ab50458712
AtosAiresArruda <atos.a.arruda@gmail.com>
2025-11-11 20:51:01 -0300

    Ajuste na organização das pastas

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Cenas/mundo.tscn")
+	get_tree().change_scene_to_file("res://Cenas/Fases/mundo.tscn")
 
 
 func _on_options_pressed() -> void:
